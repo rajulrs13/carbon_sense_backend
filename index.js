32e26bee6b7a4420158c31f65d9444a97d329dff
Rajul <rajulrs13@gmail.com>
2023-03-20 14:50:03 +0530

    update'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const calculate = require("./calculate2");
+const calculate = require("./api/calculate");
 
 app.use(express.json({ extended: false }));
 
